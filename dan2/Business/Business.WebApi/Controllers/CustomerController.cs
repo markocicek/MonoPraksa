830f21482314872544da4dff0a0f1a816c79f7d2
Marko Cicek <marko.cicek0@gmail.com>
2023-01-17 16:02:36 +0100

    WebApi

---
+++ dan2/Business/Business.WebApi/Controllers/CustomerController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.Ajax.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.UI.WebControls;
+
+namespace Business.WebApi.Controllers
+{
+    public class CustomerController : ApiController
+    {
+        public static List<Customer> customers = new List<Customer> { new Customer("Marko", "Cicek", 1),
+            new Customer("Ivan", "Ivanic", 2), 
+            new Customer("Kristijan", "Kristijanic", 3) };
+
+        [HttpGet]
+        [Route("api/customer/all")]
+        public IEnumerable<Customer> ListCustomers()
+        {
+            return customers;
+        }
+        [HttpGet]
+        public HttpResponseMessage Find(int id)
+        {
+            Customer localCustomer = customers.Find(customer => customer.Id == id);
+            if(localCustomer == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound, id);
+            }
+            return Request.CreateResponse(HttpStatusCode.OK, localCustomer);
+        }
+        [HttpPost]
+        public HttpResponseMessage Add([FromBody]Customer customer)
+        {
+            if(customer.Fname == null || customer.Lname == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, customer.Id);
+            }
+            customer.Id = customers.Last().Id + 1;
+            customers.Add(customer);
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+
+        [HttpPut]
+        public HttpResponseMessage Update(int id, [FromBody]Customer updatedCustomer)
+        {
+            Customer localCustomer = customers.Find(customer => customer.Id == id);
+            if (localCustomer == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound, id);
+            }
+            else if (updatedCustomer.Fname == null || updatedCustomer.Lname == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, updatedCustomer.Id);
+            }
+            customers.ElementAt(id-1).Fname = updatedCustomer.Fname;
+            customers.ElementAt(id-1).Lname = updatedCustomer.Lname;
+
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+
+        [HttpDelete]
+        public HttpResponseMessage Delete(int id)
+        {
+            if (!customers.Remove(customers.Find(customer => customer.Id == id)))
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound, id);
+            }
+            
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+        
+    }
+}
---
+++ dan2/Business/Business.WebApi/Customer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Business.WebApi
+{
+    public class Customer
+    {
+        private string fname;
+        private string lname;
+        private int id;
+
+        public string Fname { get { return fname; } set { fname = value; } }
+        public string Lname { get { return lname; } set { lname = value; } }
+        public int Id { get { return id; }set { id = value; } }
+
+        public Customer(string fname, string lname, int id)
+        {
+            this.fname = fname;
+            this.lname = lname;
+            this.id = id;
+        }
+    }
+}
